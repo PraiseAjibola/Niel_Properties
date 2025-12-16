7b3c24ba7c0f559e5cc40eee383e8742c225ad84
Praise Ajibola <prayzajibola@gmail.com>
2025-12-16 05:04:36 +0100

    Added Hash Router to the main

--- niel_properties/src/main.jsx
+++ niel_properties/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// 1. Import HashRouter from react-router-dom
+import { HashRouter } from 'react-router-dom'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* 2. Wrap your App component with HashRouter */}
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
