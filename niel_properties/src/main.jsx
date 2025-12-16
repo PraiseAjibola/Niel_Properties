d839057927e96505d6e140492b96d193b4127d74
Praise Ajibola <prayzajibola@gmail.com>
2025-12-16 05:06:51 +0100

    Removed hash router

--- niel_properties/src/main.jsx
+++ niel_properties/src/main.jsx
@@ -3,14 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// 1. Import HashRouter from react-router-dom
-import { HashRouter } from 'react-router-dom'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* 2. Wrap your App component with HashRouter */}
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </StrictMode>,
 )
