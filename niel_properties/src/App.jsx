6ccd60c9471754397a666264cc0f618cfc09c855
Praise Ajibola <prayzajibola@gmail.com>
2025-12-16 05:17:04 +0100

    Updated the App.jsx

--- niel_properties/src/App.jsx
+++ niel_properties/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// 1. Import ONLY Routes and Route (Do not import Router or BrowserRouter)
+import { Routes, Route } from 'react-router-dom';
 
 // Import your page components
 import Home from './Pages/Home';
-import SignUp from './Pages/SignUp'; // Ensure you have this file created
+import SignUp from './Pages/SignUp'; 
 import Login from './Pages/Login';
 import WelcomeScreen from './Pages/WelcomeScreen';
 import RoleSelection from './Pages/RoleSelection';
@@ -14,24 +15,23 @@ import SuccessPage from './Pages/SuccessPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Route for the Landing Page */}
-        <Route path="/" element={<Home />} />
-        
-        {/* Route for the Sign Up Page */}
-        <Route path="/signup" element={<SignUp />} />
-        
-        {/* You can add a Login route later */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/welcome" element={<WelcomeScreen />} />
-        <Route path="/role" element={<RoleSelection />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/check-email" element={<CheckEmail />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/success" element={<SuccessPage />} />
-      </Routes>
-    </Router>
+    // 2. DELETE <Router> here. Just start with <Routes>
+    <Routes>
+      {/* Route for the Landing Page */}
+      <Route path="/" element={<Home />} />
+      
+      {/* Route for the Sign Up Page */}
+      <Route path="/signup" element={<SignUp />} />
+      
+      <Route path="/login" element={<Login />} />
+      <Route path="/welcome" element={<WelcomeScreen />} />
+      <Route path="/role" element={<RoleSelection />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/check-email" element={<CheckEmail />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/success" element={<SuccessPage />} />
+    </Routes>
+    // 3. DELETE </Router> here
   );
 }
 
